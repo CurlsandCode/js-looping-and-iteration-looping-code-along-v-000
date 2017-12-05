0ca60bf507c2c0a949685f17436edb302268dc48
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 10:04:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
 
-function printBadges () {
-  for (let i = 0; i < printBadges.length; i++) {
-    console.log(`Welcome ${printBadges[i]}! You are employee #${i}`);
+function printBadges (name) {
+  for (let i = 0; i < name.length; i++) {
+    console.log(`Welcome ${name[i]}! You are employee #${i}`);
   }
 
-  return printBadges;
+  return name;
 }
