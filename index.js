a4e468316f9180bc768d15455c38d1dab437c50b
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 10:16:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 
 function printBadges (names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}`);
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
 
   return names;
 }
 
 function tailsNeverFails () {
-  let count = 0;
+  let tails = 0;
 
-  while (Math.random() >=0.5) {
-   count++;
-}
-return 'You got ${count} tails in a row!';
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
 }
