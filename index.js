97cf413f947a1963e92f1a29fbc2c87c80c2474c
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 10:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-
+const name = [];
 function printBadges (name) {
   for (let i = 0; i < name.length; i++) {
     console.log(`Welcome ${name[i]}! You are employee #${i}`);
