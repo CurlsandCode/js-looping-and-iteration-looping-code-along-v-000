9aef853328ad979de118a53438efc95319952d88
Monika Williams <monika.m.williams@gmail.com>
2017-12-05 10:06:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
-const name = [];
-function printBadges (name) {
-  for (let i = 0; i < name.length; i++) {
-    console.log(`Welcome ${name[i]}! You are employee #${i}`);
+const printBadges = [];
+function printBadges () {
+  for (let i = 0; i < printBadges.length; i++) {
+    console.log(`Welcome ${printBadges[i]}! You are employee #${i}`);
   }
 
-  return name;
+  return printBadges;
 }
